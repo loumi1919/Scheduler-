9e4205927327f2243ac00b331b070a24b25b5b6d
Loumi1919 <gueyeloumi@gmail.com>
2025-04-26 19:47:01 +0200

    add api feat

--- availability/views.py
+++ availability/views.py
@@ -9,19 +9,3 @@ from .models import Event
 
 def scheduler(request):
     return render(request, 'scheduler.html')
-
-
-@csrf_exempt
-def scheduler_data(request):     
-    out = [
-        {
-            'title': event.description,
-            'id': event.id,
-            'date': event.date,
-            'start': datetime.datetime.combine(event.date, event.start),
-            'end': datetime.datetime.combine(event.date, event.end),
-            'status':event.status
-        } for event in Event.objects.all()
-    ]
-  
-    return JsonResponse(out, safe=False)
